b0bc87c830b87a2a83b9a05cdc994f0019f88170
Kim Nanyoung <nanyoung18@gmail.com>
2020-08-24 16:42:51 +0900

    USING unordered_multimap (중복되면 break 되는 문제)

--- 프로그래머스_스킬테스트/마라톤.cpp
+++ 프로그래머스_스킬테스트/마라톤.cpp
@@ -1,40 +1,52 @@
 #include <string>
-#include <vector>
+#include <unordered_map>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
 	string answer = "";
-	bool chk = true;
-	bool chk2 = true;
-
+	
+	unordered_multimap<string, int>::iterator it;
+	unordered_multimap<string, int>::iterator chk;
+	unordered_multimap<string, int> newparticipant;
 
 	for (int i = 0; i < participant.size(); i++) {
-		bool chk = true;
-		bool chk2 = true;
-		for (int j = 0; j < completion.size(); j++) {
-		
-			if (participant[i] != completion[j]) {
-				chk = false;
-				//cout << participant[i] << " " << completion[j] << endl;
-			}
-			else if((participant[i] == completion[j])){
-				chk = true;
-				//cout << participant[i] << " " << completion[j] << endl;
-				completion.erase(completion.begin() + j, completion.begin() + j + 1);
-				break;
-			}
-
-			if ((chk == false) && (j == (completion.size() - 1))) {
-				//cout << participant[i] << " " << completion[j] << endl;
-				answer = participant[i];
-				chk2 = false;
-				break;
-			}
-		}
-		if (!chk2) break;
+		newparticipant.insert(pair<string, bool>(participant[i], false));
 	}
+
+
+	
+	for (int i = 0; i < completion.size(); i++) {
+
+		chk = newparticipant.find(completion[i]);
+
+		if (chk->second == 0) {
+			chk->second += 1;	//방문했다고 체크
+			cout << "방문된 곳: " << chk->first << " ";
+
+		}
+		else {
+			
+		}
+	}
+	cout << endl;
+
+	for (it = newparticipant.begin(); it != newparticipant.end(); it++) {
+
+		cout << it->first << " " << it->second << endl;
+	}
+
+	for (it = newparticipant.begin(); it != newparticipant.end(); it++) {
+
+		if (it->second == false) {
+			
+			answer = it->first;
+		}
+
+	}
+
 
 
 	return answer;
@@ -42,8 +54,12 @@ string solution(vector<string> participant, vector<string> completion) {
 
 int main() {
 
-	vector<string> participant = { "mislav", "stanko", "mislav", "ana" };
-	vector<string> completion = {"stanko", "ana", "mislav" };
+
+	vector<string> participant = { "a", "b", "a", "c" ,"a", "d" };
+
+	vector<string> completion = {"a", "b", "a", "d", "a" };
+
+
 	
 	string ans = solution(participant, completion);
 	cout << ans << endl;
